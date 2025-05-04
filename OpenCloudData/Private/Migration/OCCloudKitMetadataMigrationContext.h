020cca0e23ea6af8d02566469734d970b1329d7f
Jinwoo Kim <kidjinwoo@me.com>
2025-05-04 21:22:11 +0900

    no message

--- OpenCloudData/Private/Migration/OCCloudKitMetadataMigrationContext.h
+++ OpenCloudData/Private/Migration/OCCloudKitMetadataMigrationContext.h
@@ -11,21 +11,22 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-#warning TODO
-
 @interface OCCloudKitMetadataMigrationContext : NSObject {
     @package NSMutableArray<NSSQLiteStatement *> *_migrationStatements; // 0x8
     @package NSMutableArray<NSSQLEntity *> *_sqlEntitiesToCreate; // 0x10
     @package NSMutableSet<NSSQLEntity *> *_constrainedEntitiesToPreflight; // 0x18
     @package BOOL _hasWorkToDo; // 0x20
     @package BOOL _needsMetdataMigrationToNSCKRecordMetadata; // 0x21
+    @package BOOL _needsMirroredRelationshipsLinkedToZone; // 0x23
     @package BOOL _needsOldTableDrop; // 0x22
     @package BOOL _needsImportAfterClientMigration; // 0x24
     @package BOOL _needsBatchUpdateForSystemFieldsAndLastExportedTransaction; // 0x25
     @package BOOL _needsAnalyzedHistoryCheck; // 0x26
+    @package BOOL _needsCleanupFromNeedsNewShareInvitationBug; // 0x27
     @package BOOL _needsCleanupFromOrphanedMirroredRelationships; // 0x28
     @package NSManagedObjectModel *_currentModel; // 0x30
     @package NSSQLModel *_sqlModel; // 0x38
+    @package NSManagedObjectModel *_storeMetadataModel; // 0x40
     @package NSSQLModel *_storeSQLModel; // 0x48
     @package NSNumber *_storeMetadataVersion; // 0x50
     @package NSDictionary *_storeMetadataVersionHashes; // 0x58
